799d0f2cf2d1051499c4817ed182cc0dbf390bbf
Modith hadya leelananda <modith.hadya@gmail.com>
2020-08-13 18:18:41 +0300

    init

---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 1,
+  };
+  render() {
+    return (
+      <div>
+        <span style={} className="badge badge-primary m-2">
+          {this.formatcounter()}
+        </span>
+        <button className="btn btn-outline-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  formatcounter() {
+    const { count } = this.state;
+    return count === 0 ? count : <h1>Not Zero</h1>;
+  }
+}
+export default Counter;
